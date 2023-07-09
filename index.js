1fc27cc8b51d208172d1bad43cfaefcd1026583d
Vikram Navik <vikramnavik98@gmail.com>
2023-07-09 23:40:03 +0530

    stat section done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const line1 = document.getElementById('line1');
 const line2 = document.getElementById('line2');
+const resume_link = `./doc/Vikram_Resume.pdf`;
 
 const line1AnimationDuration = 1000; // Animation duration for line1 in milliseconds
 
@@ -13,3 +14,63 @@ setTimeout(() => {
   line2.style.animation = 'typing 3s steps(30), blink-caret 0.5s step-end infinite';
   line2.style.display = 'inline-block';
 }, line1AnimationDuration);
+
+// Resume Button opening on new tab function
+
+document.getElementById("resumeButton").addEventListener("click", ()=>{
+  window.open(resume_link);
+})
+
+// My Github Calendar
+const username = 'your-github-username';
+const token = 'your-personal-access-token';
+
+// Fetch your GitHub contributions data using GraphQL
+const query = `
+  query {
+    user(login: "${username}") {
+      contributionsCollection {
+        contributionCalendar {
+          totalContributions
+          weeks {
+            contributionDays {
+              contributionCount
+              date
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+fetch('https://api.github.com/graphql', {
+  method: 'POST',
+  headers: {
+    'Authorization': `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ query }),
+})
+  .then(response => response.json())
+  .then(data => {
+    const contributions = data.data.user.contributionsCollection.contributionCalendar;
+
+    // Create a new calendar element
+    const calendar = document.createElement('div');
+    calendar.id = 'github-calendar';
+    calendar.classList.add('github-calendar');
+
+    // Generate the calendar HTML using the contributions data
+    let calendarHTML = '';
+    contributions.weeks.forEach(week => {
+      week.contributionDays.forEach(day => {
+        calendarHTML += `<div data-count="${day.contributionCount}" data-date="${day.date}"></div>`;
+      });
+    });
+    calendar.innerHTML = calendarHTML;
+
+    // Append the calendar to the container element
+    const container = document.getElementById('github-calendar');
+    container.appendChild(calendar);
+  });
